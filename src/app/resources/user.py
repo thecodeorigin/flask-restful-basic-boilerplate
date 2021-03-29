c3c25185853b30ada362f69f5c88247aa625f359
thangdo19 <18thangd@gmail.com>
2021-03-29 16:21:08 +0700

    :sparkles: Showed how to add claims(jwt payload)

--- src/app/resources/user.py
+++ src/app/resources/user.py
@@ -2,13 +2,14 @@ import sqlite3
 
 from flask_restful import Resource, reqparse
 from bcrypt import hashpw, gensalt
-from flask_jwt_extended import jwt_required
+from flask_jwt_extended import jwt_required, get_jwt
 
 from ..models.user import UserModel
 
 class UserGetMany(Resource):
   @jwt_required()
   def get(self):
+    # How to get claims(jwt payload): claims = get_jwt()
     users = [user.toDict() for user in UserModel.query.all()]
 
     return { 'data': users }
--- src/config/jwt.py
+++ src/config/jwt.py
@@ -2,3 +2,9 @@ from flask_jwt_extended import JWTManager
 
 def config_jwt(app):
   jwt = JWTManager(app)
+
+  @jwt.additional_claims_loader
+  def add_claims(identity):
+    # if identity['id'] == 1:
+    #   return { 'is_admin': True }
+    return { 'is_admin': False }
